dd6929213d644a15973cb28eb305ab83dd719a85
Umair Arshad <umair6622@gmail.com>
2021-04-18 00:49:40 +0500

    Add comonent with arrow function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-function Greeting() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+const Greeting = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h1',{},'Hello World')
   );
-}
+};
+
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
