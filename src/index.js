6ccfaf76367bfd25799f519b7071bd6ae5cee6b5
Umair Arshad <umair6622@gmail.com>
2021-04-18 15:17:19 +0500

    Add the nested components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// function Greeting() {
-//   return (
-//     <div>
-//       <h1>Hello World</h1>
-//     </div>
-//   );
-// }
-
-const Greeting = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1',{},'Hello World')
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
   );
-};
+}
 
-
+const Person = () => <h2>Umair Arshad</h2>
+const Message = () =>{
+  return <p>This is my message.</p>
+}
 ReactDom.render(<Greeting/>, document.getElementById('root'));
