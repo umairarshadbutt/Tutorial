cbbc213f1a9c87f85a9ccdd991380efed7c97d54
Umair Arshad <umair6622@gmail.com>
2021-04-18 17:07:39 +0500

    Update the book

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@ import ReactDom from 'react-dom';
 function Greeting() {
   return (
     <div>
-      <Person />
-      <Message />
+      <Book />
     </div>
   );
 }
 
-const Person = () => <h2>Umair Arshad</h2>
-const Message = () =>{
-  return <p>This is my message.</p>
+
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
 }
+const Image = () =>{
+ return <img 
+  src='https://javedahmedghamidi.org/m/thumb/book/bf51770e-42df-46a4-bbf4-5dcd8d1047b0.jpg' 
+  alt=''/>
+}
+
+const Title = () => <h1>Nakhun </h1>
+const Author = () => <h2>Amir Hazdar</h2>
 ReactDom.render(<Greeting/>, document.getElementById('root'));
