7e5188a889b8ead2895850d17f8149d1ed782659
Umair Arshad <umair6622@gmail.com>
2021-04-20 02:20:10 +0500

    Proper List

--- src/index.js
+++ src/index.js
@@ -3,24 +3,30 @@ import ReactDom from 'react-dom';
 
 import './index.css'
 
-const firstBook = {
-  img: 'https://javedahmedghamidi.org/m/thumb/book/bf51770e-42df-46a4-bbf4-5dcd8d1047b0.jpg',
-  title: 'Qurbani sy Pehly',
-  author: 'Amir Gazdar'
-};
-
-const secondBook = {
-  img: 'https://javedahmedghamidi.org/m/thumb/book/ab0b2b84-21d2-4294-80bd-faba96b2439d.jpg',
-  title: 'Introduction to the Qur’ān ',
-  author: 'Dr Shehzaad Saleem'
-};
+const books = [
+  {
+    img: 'https://javedahmedghamidi.org/m/thumb/book/bf51770e-42df-46a4-bbf4-5dcd8d1047b0.jpg',
+    title: 'Qurbani sy Pehly',
+    author: 'Amir Gazdar'
+  },
+  {
+    img: 'https://javedahmedghamidi.org/m/thumb/book/ab0b2b84-21d2-4294-80bd-faba96b2439d.jpg',
+    title: 'Introduction to the Qur’ān ',
+    author: 'Dr Shehzaad Saleem'
+  },
+  {
+    img: 'https://javedahmedghamidi.org/m/thumb/book/bf51770e-42df-46a4-bbf4-5dcd8d1047b0.jpg',
+    title: 'Qurbani sy Pehly',
+    author: 'Amir Gazdar'
+  },
+];
 function Greeting() {
   return (
     <div className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-        <p>lorem  lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+      {books.map((book) =>{
+        const {img, title, author} = book;
+        return <Book book={book}></Book>
+      })}
     </div>
   );
 }
@@ -28,13 +34,12 @@ function Greeting() {
 
 
 const Book = (props) => {
-  const {img, title, author, children} = props;
+  const {img, title, author} = props.book;
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 }
